545814c0e58e26340d584d6bcef2ebe227d6efcd
Arpan Dutta <arpandutta639@gmail.com>
2025-07-25 22:23:07 +0530

    weather api added

---
+++ lib/data/repository/weather_fetch.dart
@@ -0,0 +1,25 @@
+import 'package:dio/dio.dart';
+import 'package:weather_bloc/data/secrets/secrets.dart';
+
+class WeatherFetch {
+  final dio = Dio();
+  Future<Map<String, dynamic>> fetchWeatherData(String cityName) async {
+    try {
+      final response = await dio.get(
+        'https://api.openweathermap.org/data/2.5/forecast',
+        queryParameters: {
+          'queryCity': cityName,
+          'appid': openWeatherAPIKey,
+          'units': 'metric',
+        },
+      );
+      if (response.statusCode == 200) {
+        return response.data;
+      } else {
+        throw Exception('Failed to load weather data');
+      }
+    } catch (e) {
+      throw Exception('Failed to load weather data: $e');
+    }
+  }
+}
---
+++ lib/data/secrets/secrets.dart
@@ -0,0 +1,1 @@
+const openWeatherAPIKey = '9769b662cec5aa8e0f172cb6b688bf40';
